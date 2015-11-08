00ee7989f97fba88d7f4176398fd2a65bd4a98a3
Vitaliy V <neural.null@gmail.com>
2015-11-08 16:57:10 +0300

    SPA arch improved

--- public/js/viewModel.js
+++ public/js/viewModel.js
@@ -3,101 +3,142 @@ ko.applyBindings(new function () {
 
   // Title
   self.title = ko.observable();
-  // Chosed page
-  self.page = ko.observable();
+  // Page architecture
+  self.page = {
+    pageName: ko.observable(),
+    content: ko.observable(),
+    // List controls
+    controls: ko.observable(),
+    // List view model (simpleGrid VM)
+    viewModel: ko.observable(),
+    // Chosed item in list
+    selected: ko.observable()
+  };
+  // Chosed action
+  self.action = ko.observable();
 
-  // Employee list
-  self.employees = ko.observableArray();
-  // Post list
-  self.posts = ko.observableArray();
+  // Data lists
+  self.lists = {
+    // Employee list
+    employees: ko.observableArray(),
+    // Post list
+    posts: ko.observableArray()
+  };
 
-  // Chosed item in list
-  self.selected = ko.observable();
   // So... This is intermodel intagrating (for simpleGrid view template)
-  self.employees.selected = self.selected;
-  self.posts.selected = self.selected;
-
+  // Selected item in list (linked to main view model)
+  for (var list in self.lists) self.lists[list].selected = self.page.selected;
   // Sorting parameters
-  self.employees.sortParams = ko.observable({ field: 'formattedName'/* , desc: false */ });
-  self.posts.sortParams = ko.observable({ field: 'value' });
+  self.lists.employees.sortParams = ko.observable({ field: 'formattedName'/* , desc: false */ });
+  self.lists.posts.sortParams = ko.observable({ field: 'value' });
 
-  // Employee list view model
-  self.employeeGridModel = new ko.simpleGrid.viewModel({
-    // simpleGrid basic parameters
-    data: self.employees,
-    columns: [
-      { headerText: 'ФИО', rowText: 'formattedName' },
-      { headerText: 'Должность', rowText: 'post' },
-      { headerText: 'Возраст', rowText: 'age' }
-    ],
-    pageSize: 10
-  });
-  // Post list view model
-  self.postGridModel = new ko.simpleGrid.viewModel({
-    data: self.posts,
-    columns: [
-      { headerText: 'Название', rowText: 'value' }
-    ],
-    pageSize: 10
-  });
-  // List view model depending on chosen view 
-  // (not undefined due to some errors when app initialized)
-  self.listViewModel = ko.observable(self.employeeGridModel);
+  // Page parameters
+  self.pages = {
+    'main': {
+      title: 'Главная страница',
+      content: 'Добро пожаловать!'
+    },
 
-  // These functions get data from server via AJAX
-  self.getEmployees = function() {
-    $.getJSON('/api/employees', function (data) { // Get array of every employee parameters
-      self.employees($.map(data, function (item) { // Update self.employees by array of Employee objects
-        return new Employee(item);
-      }));
-      self.employees.sortByRowText(); // Initial sort
-    });
+    'employees': {
+      title: 'Список сотрудников',
+      controls: {
+        // Update function gets data from server via AJAX
+        update: function() {
+          $.getJSON('/api/employees', function (data) { // Get array of every employee parameters
+            self.lists.employees($.map(data, function (item) { // Update self.employees by array of Employee objects
+              return new Employee(item);
+            }));
+            self.lists.employees.sortByRowText(); // Initial sort
+          });
+        },
+        remove: function() {
+
+        }
+      },
+      // Employee list view model
+      viewModel: new ko.simpleGrid.viewModel({
+        // simpleGrid basic parameters
+        data: self.lists.employees,
+        columns: [
+          { headerText: 'ФИО', rowText: 'formattedName' },
+          { headerText: 'Должность', rowText: 'post' },
+          { headerText: 'Возраст', rowText: 'age' }
+        ],
+        pageSize: 10
+      })
+    },
+
+    'posts': {
+      title: 'Список должностей',
+      controls: {
+        update: function() {
+          // Get'n'Renew
+          $.getJSON('/api/posts', function(data) {
+            self.lists.posts(data);
+            self.lists.posts.sortByRowText();
+          });
+        },
+        remove: function() {
+
+        }
+      },
+      // Post list view model
+      viewModel: new ko.simpleGrid.viewModel({
+        data: self.lists.posts,
+        columns: [
+          { headerText: 'Название', rowText: 'value' }
+        ],
+        pageSize: 10
+      })
+    }
   };
-  self.getPosts = function() {
-    $.getJSON('/api/posts', function(data) {
-      self.posts(data);
-      self.posts.sortByRowText();
-    });
+
+  self.actions = {
+    'add': {
+    },
+    'edit': {
+    }
   };
-  // Update function
-  self.update = ko.observable();
-  self.remove = ko.observable();
 
   // Sammy router initialize
   self.router = Sammy(function() {
-    this.get('#main', function() {
-      self.title('Главная страница');
-      self.page('main');
+    this.get('/\#\/(main|employees|posts)/', function() {
+      var pageName = this.params.splat[0];
+      var page = self.pages[pageName];
+
+      self.title(page.title);
+      self.page.pageName(pageName);
+      self.page.content(page.content);
+      self.page.controls(page.controls);
+      if (page.controls) page.controls.update();
+      self.page.viewModel(page.viewModel);
+      self.page.selected(undefined);
     });
 
-    this.get('#employees', function() {
-      self.title('Список сотрудников');
-      self.page('employees');
-      self.update(self.getEmployees);
-      self.getEmployees();
-      self.listViewModel(self.employeeGridModel);
-    });
+    this.get('/\#\/(employees|posts)\/(add|edit\/[0-9]+)/', function() {
+      var result = this.params.splat;
+      var pageName = result[0];
+      var actionName = result[1].split('/')[0];
+      var id = result[1].split('/')[1];
 
-    this.get('#posts', function() {
-      self.title('Список должностей');
-      self.page('posts');
-      self.update(self.getPosts);
-      self.getPosts();
-      self.listViewModel(self.postGridModel);
-    });
+      if (self.page.pageName() != pageName) {
+        if (id) {
+          self.lists[pageName].subscribe(function() {
+            var item = self.page.viewModel().data().filter(function(el) {
+              return el.id == id;
+            })[0];
 
-    this.get('#:page/add', function() {
-      if (self.page() != this.params.page)
-        this.app.runRoute('get', '#' + this.params.page);
-    });
+            if (item) self.page.selected(item.id);
+            this.dispose();
+          });
+        }
 
-    this.get('#:page/edit', function() {
-      if (self.page() != this.params.page)
-        this.app.runRoute('get', '#' + this.params.page);
+        this.app.runRoute('get', '/#/' + pageName + '/');
+      }
     });
 
     // Main page is main page
-    this.get('', function() { this.app.runRoute('get', '#main') });
+    this.get('', function() { this.app.runRoute('get', '/#/main/') });
   })
 
   self.router.run();
